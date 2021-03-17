4020ab12fe9f2cdef8b2b5ae286c115119791f80
Revanth-M <revanthkumar.makkena@gmail.com>
2021-03-16 19:50:55 -0500

    Bank account problem

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -6,8 +6,8 @@ class Matrix:
         self.raw_string = matrix_string
         self.total_rows = self.calculateTotalRows()
         self.total_columns = self.calculateTotalColumns()
-        # print(f"rows are {self.total_rows}")
-        # print(f"columns are {self.total_columns}")
+        print(f"rows are {self.total_rows}")
+        print(f"columns are {self.total_columns}")
 
     def row(self, index):
         print(self.raw_string[self.total_rows*index-1:self.total_rows+2])
@@ -29,8 +29,8 @@ class Matrix:
         return(count)
 
 
-x = Matrix("1 2 3\n4 5 6\n7 8 9")
-x.row(2)
+x = Matrix("1 2 e 4\n4 5 4 5")
+# x.row(2)
 # 1 2 3
 # 4 5 6
 # 7 8 9
